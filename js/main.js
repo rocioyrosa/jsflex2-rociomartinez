e899d742eaa81ab34b3f68fd2c75f65e7be5fe04
rocioyrosa <rociomartinezcomunicacion@gmail.com>
2023-10-27 21:00:29 -0300

    eliminar comentarios de js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Se ejecuta cuando el DOM se ha cargado completamente
-
-    // Obtiene del DOM 'viajesSection'
     const viajesSection = document.getElementById('viajesSection');
-
-    // Obtiene del almacenamiento local los viajes calculados, si no hay, asignar un array vacío
     const viajes = JSON.parse(localStorage.getItem('viajes')) || [];
 
     if (viajes.length > 0) {
-        // Si hay viajes, los muestra y muestra la sección 'viajesSection' en bloque
         mostrarViajes();
         viajesSection.style.display = 'block';
     } else {
-        // Si no hay viajes, oculta la 'viajesSection' y comienza en 1 sola screenview
         viajesSection.style.display = 'none';
     }
 });
 
 document.getElementById('viajeForm').addEventListener('submit', function (event) {
-    // Se ejecuta cuando el user envía formulario de viaje
+    event.preventDefault();
 
-    event.preventDefault(); // Previene el comportamiento por defecto: recarga de página en el navegador tras la acción de submit Calcular.
-
-    // Obtiene los valores del formulario parseados en números decimales
     const distanciaViaje = parseFloat(document.getElementById('distancia').value);
     const precioCombustible = parseFloat(document.getElementById('precioCombustible').value);
     const rendimientoAuto = parseFloat(document.getElementById('rendimientoAuto').value);
 
     if (!isNaN(distanciaViaje) && !isNaN(precioCombustible) && !isNaN(rendimientoAuto) && rendimientoAuto !== 0) {
-        // Verifica si los valores son válidos
-
-        // Calcula la cantidad de litros necesarios y el costo total para cada viaje
         const litrosNecesarios = distanciaViaje / rendimientoAuto;
         const costoTotal = litrosNecesarios * precioCombustible;
 
-        // Actualiza el DOM con el resultado en una case de alerta de BS, que tmb arregla el resultado final a decimal de 2 cifras.
         const resultadoContainer = document.getElementById('resultado');
         resultadoContainer.innerHTML = `<div class= "alert alert-success"><p>El costo total del combustible para tu viaje es de $${costoTotal.toFixed(2)}</p></div>`;
 
-        // Objeto con propiedad y valor (variables) de los detalles del viaje
         const viaje = {
             distancia: distanciaViaje,
             precioLitro: precioCombustible,
@@ -46,39 +31,29 @@ document.getElementById('viajeForm').addEventListener('submit', function (event)
             costoTotal: costoTotal
         };
 
-        // Array que obtiene los objetos 'viaje' existentes en el almacenamiento local o asigna un array vacío en el caso de que no lo haya. 
         const viajes = JSON.parse(localStorage.getItem('viajes')) || [];
         viajes.push(viaje);
 
-        // Guarda los viajes actualizados en el almacenamiento local
         localStorage.setItem('viajes', JSON.stringify(viajes));
 
-        // Muestra la lista actualizada de viajes
         mostrarViajes();
     } else {
-        // Si los valores no son válidos, muestra un mensaje de error, aunque es muy improbable que salga porque el form tiene validación desde BS, pero es una segunda capa para el correcto funcionamiento del cicloo.
         const resultadoContainer = document.getElementById('resultado');
         resultadoContainer.innerHTML = `<div class= "alert alert-danger"><p>Hubo un error en tu cálculo</p></div>`;
     }
 });
 
 function mostrarViajes() {
-    // Función para mostrar la lista de viajes
-
-    // Obtiene los viajes del almacenamiento local o asigna un array vacío si no hay
     const viajes = JSON.parse(localStorage.getItem('viajes')) || [];
     const viajesContainer = document.getElementById('viajesContainer');
     const borrarHistorialBtn = document.getElementById('borrarHistorialBtn');
     const viajesTitle = document.getElementById('viajesTitle');
     const viajesSection = document.getElementById('viajesSection')
 
-
-    viajesContainer.innerHTML = ''; // Limpia el contenido actual para que no se dupliquen las cards de viajes
+    viajesContainer.innerHTML = '';
 
     if (viajes.length > 0) {
         viajes.forEach((viaje, index) => {
-            // Para cada viaje, si hay, crea una tarjeta (card) con la información
-
             const card = document.createElement('div');
             card.classList.add('card', 'm-3', 'col');
 
@@ -96,17 +71,16 @@ function mostrarViajes() {
                 <button class="btn btn-danger" onclick="borrarViaje(${index})">Borrar</button></div>
             </li>
                 </div>
-            `; // En cada card que se crea se agrega el botón 'Borrar' con el atributo/evento onclick que al llamar a la función borrarViaje() y haciendo refe a ese array que se está recorriendo lo elimina del local storage.
+            `;
 
-            viajesContainer.appendChild(card); // Agregar la nueva card (hija) al contenedor padre
+            viajesContainer.appendChild(card);
         });
 
-        borrarHistorialBtn.style.display = 'block'; // Asegura que el botón y el título de la section se muestren en bloques
+        borrarHistorialBtn.style.display = 'block';
         viajesTitle.style.display = 'block';
         viajesSection.style.display = 'block';
 
     } else {
-        // Si no hay viajes, muestra un mensaje que lo indica y oculta botón y título 
         borrarHistorialBtn.style.display = 'none';
         viajesTitle.style.display = 'none';
 
@@ -134,17 +108,17 @@ function borrarHistorial() {
         });
 
     const viajesContainer = document.getElementById('viajesContainer');
-    viajesContainer.innerHTML = ''; // Llama a viajesContainer y limpiar el contenido actual 
+    viajesContainer.innerHTML = '';
 
     const viajesTitle = document.getElementById('viajesTitle');
-    viajesTitle.style.display = 'none'; // Llama a viajesTitle y lo oculta
+    viajesTitle.style.display = 'none';
 
     const borrarHistorialBtn = document.getElementById('borrarHistorialBtn');
-    borrarHistorialBtn.style.display = 'none'; // Llama al botón de borrar historial y lo oculta
+    borrarHistorialBtn.style.display = 'none';
 
     const noViajesMessage = document.createElement('p');
     noViajesMessage.innerText = 'No hay viajes guardados.';
-    viajesContainer.appendChild(noViajesMessage); // Crea y muestra un mensaje indicando que no hay viajes guardados
+    viajesContainer.appendChild(noViajesMessage);
 }
 
 function borrarViaje(index) {
@@ -167,9 +141,7 @@ function borrarViaje(index) {
     });
 }
 
-mostrarViajes(); // Muestra los viajes al cargar la página 
-
-
+mostrarViajes();
 
 async function obtenerPronostico(ciudad) {
     const apiKey = '80c663c79ca93ee7fe3b778abfd7c9a0'; 
@@ -193,12 +165,10 @@ async function obtenerPronosticoExtendido(ciudad) {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        // Filtrar los datos para obtener el pronóstico de los próximos días
         const pronosticoExtendido = data.list.filter(item => item.dt_txt.includes('12:00:00'));
 
-        // Formatear los datos para que sean más fáciles de usar
         const pronosticoFormateado = pronosticoExtendido.map(item => {
-            const fecha = new Date(item.dt * 1000); // Convertir timestamp a milisegundos
+            const fecha = new Date(item.dt * 1000);
             const options = { weekday: 'long', day: 'numeric' };
             const fechaFormateada = fecha.toLocaleDateString('es-ES', options);
 
